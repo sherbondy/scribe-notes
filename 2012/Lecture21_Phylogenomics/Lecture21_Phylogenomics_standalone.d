1109a43d7597a963147189d9ef72fbb33f2ef166
Ethan Sherbondy <sherbondye@gmail.com>
2012-11-13 18:46:52 -0500

    initial commit of the notes from 2011.

---
+++ 2012/Lecture21_Phylogenomics/Lecture21_Phylogenomics_standalone.d
@@ -0,0 +1,109 @@
+# vim: ft=make
+.PHONY: Lecture21_Phylogenomics_standalone._graphics
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,../../templates/macros.tex)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,../../templates/preamble.tex)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/ifxetex/ifxetex.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/gettitlestring.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/hobsub-generic.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/hobsub-hyperref.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/infwarerr.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/ltxcmds.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amscls/amsthm.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsfonts/amsfonts.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsfonts/amssymb.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsbsy.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsgen.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsmath.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsopn.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amstext.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/base/article.cls)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/base/makeidx.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/base/textcomp.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/caption/caption.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/caption/caption3.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/caption/subcaption.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/cite/chapterbib.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/fancyhdr/fancyhdr.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/float/float.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/fncychap/fncychap.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/geometry/geometry.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/color.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/graphics.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/graphicx.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/keyval.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/trig.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/hyperref/hyperref.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/hyperref/nameref.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/listings/listings.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/listings/lstmisc.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/minitoc/minitoc.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/minitoc/mtcmess.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/natbib/natbib.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/epstopdf-base.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/grfext.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/kvoptions.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/rerunfilecheck.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/tools/enumerate.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/url/url.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/wrapfig/wrapfig.sty)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,Lecture21_Phylogenomics.tex)
+Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make Lecture21_Phylogenomics_standalone.d Lecture21_Phylogenomics_standalone.pdf: $(call path-norm,Lecture21_Phylogenomics_standalone.tex)
+.SECONDEXPANSION:
+-include images/Fig01_HeuristicTreeSearch.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig01_HeuristicTreeSearch.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig01_HeuristicTreeSearch.png)
+-include images/Fig02_MLScoringStep.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig02_MLScoringStep.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig02_MLScoringStep.png)
+-include images/Fig03_SiteEvolution.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig03_SiteEvolution.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig03_SiteEvolution.png)
+-include images/Fig04_SequenceEvolution.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig04_SequenceEvolution.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig04_SequenceEvolution.png)
+-include images/Fig05_SequenceEvolutionOverTree.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig05_SequenceEvolutionOverTree.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig05_SequenceEvolutionOverTree.png)
+-include images/Fig06_PeelingAlgorithm.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig06_PeelingAlgorithm.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig06_PeelingAlgorithm.png)
+-include images/Fig07_GeneFamilyEvolution.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig07_GeneFamilyEvolution.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig07_GeneFamilyEvolution.png)
+-include images/Fig08_MaximumParsimonyReconciliation.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig08_MaximumParsimonyReconciliation.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig08_MaximumParsimonyReconciliation.png)
+-include images/Fig09_MPRAlgorithm.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig09_MPRAlgorithm.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig09_MPRAlgorithm.png)
+-include images/Fig10_LearningAcrossGeneTrees.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig10_LearningAcrossGeneTrees.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig10_LearningAcrossGeneTrees.png)
+-include images/Fig11_DevelopingRatesModel.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig11_DevelopingRatesModel.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig11_DevelopingRatesModel.png)
+-include images/Fig12_UsingRateModels.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig12_UsingRateModels.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig12_UsingRateModels.png)
+-include images/Fig13_FisherWrightModel.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig13_FisherWrightModel.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig13_FisherWrightModel.png)
+-include images/Fig14_FisherWrightManyGenerations.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig14_FisherWrightManyGenerations.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig14_FisherWrightManyGenerations.png)
+-include images/Fig15_CoalescentModel.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig15_CoalescentModel.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig15_CoalescentModel.png)
+-include images/Fig16_CoalescentProbDist.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig16_CoalescentProbDist.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig16_CoalescentProbDist.png)
+-include images/Fig17_MultispeciesCoalescent.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig17_MultispeciesCoalescent.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig17_MultispeciesCoalescent.png)
+-include images/Fig18_AlternateReconciliations.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig18_AlternateReconciliations.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig18_AlternateReconciliations.png)
+-include images/Fig19_GeneTreeInaccuracies.png.gpi.d
+Lecture21_Phylogenomics_standalone.d: $$(call graphics-source,images/Fig19_GeneTreeInaccuracies.png)
+Lecture21_Phylogenomics_standalone.pdf Lecture21_Phylogenomics_standalone._graphics: $$(call graphics-target,images/Fig19_GeneTreeInaccuracies.png)
+Lecture21_Phylogenomics_standalone.bbl Lecture21_Phylogenomics_standalone.aux Lecture21_Phylogenomics_standalone.aux.make: ./Lecture21_Phylogenomics.bib
---
+++ 2012/Lecture21_Phylogenomics/Lecture21_Phylogenomics_standalone.out
@@ -0,0 +1,12 @@
+\BOOKMARK [1][]{section.1}{Introduction}{}% 1
+\BOOKMARK [2][]{subsection.1.1}{Phylogenetics}{section.1}% 2
+\BOOKMARK [1][]{section.2}{Inferring Orthologs/Paralogs, Gene Duplication and Loss}{}% 3
+\BOOKMARK [1][]{section.3}{Learning Across Gene Trees}{}% 4
+\BOOKMARK [1][]{section.4}{Modeling Population and Allele Frequencies}{}% 5
+\BOOKMARK [1][]{section.5}{SPIDIR:Background}{}% 6
+\BOOKMARK [1][]{section.6}{SPIDIR: Method and Model}{}% 7
+\BOOKMARK [1][]{section.7}{Conclusion}{}% 8
+\BOOKMARK [1][]{section.8}{Current Research Directions}{}% 9
+\BOOKMARK [1][]{section.9}{Further Reading}{}% 10
+\BOOKMARK [1][]{section.10}{Tools and Techniques}{}% 11
+\BOOKMARK [1][]{section.11}{What Have We Learned?}{}% 12
